9964c1a0ab4cd6a98679946d9e9faa36bde79d97
SenkiAlphonse <the.nagy.kriszta@gmail.com>
2019-01-15 14:03:10 +0100

    init

---
+++ projectweek1restapi/src/main/java/com/greenfoxacademy/projectweek1restapi/Projectweek1restapiApplication.java
@@ -0,0 +1,14 @@
+package com.greenfoxacademy.projectweek1restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projectweek1restapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Projectweek1restapiApplication.class, args);
+    }
+
+}
+
