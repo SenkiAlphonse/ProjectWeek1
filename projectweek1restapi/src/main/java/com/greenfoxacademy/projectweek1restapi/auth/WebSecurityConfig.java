a817c147705f3c8ee2c065fa27f37973deac663e
SenkiAlphonse <the.nagy.kriszta@gmail.com>
2019-01-16 15:24:42 +0100

    back to REST POST, disabled xss protection, still 403

--- projectweek1restapi/src/main/java/com/greenfoxacademy/projectweek1restapi/auth/WebSecurityConfig.java
+++ projectweek1restapi/src/main/java/com/greenfoxacademy/projectweek1restapi/auth/WebSecurityConfig.java
@@ -1,3 +1,22 @@
+package com.greenfoxacademy.projectweek1restapi.auth;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends
+        WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .headers()
+                .xssProtection().xssProtectionEnabled(false);
+    }
+}
+
+
 /*package com.greenfoxacademy.projectweek1restapi.auth;
 
 import org.springframework.beans.factory.annotation.Autowired;
